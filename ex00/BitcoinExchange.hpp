824ebe9bbe365fcdb720448e45d91575afb7f421
thee-falcon <omar.makran.10@gmail.com>
2024-09-10 19:15:24 +0100

    all is done & checked alhadmollilah

--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: omakran <omakran@student.1337.ma>          +#+  +:+       +#+        */
+/*   By: omakran <omakran@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/26 20:40:00 by omakran           #+#    #+#             */
-/*   Updated: 2024/07/29 01:21:14 by omakran          ###   ########.fr       */
+/*   Updated: 2024/09/10 18:49:51 by omakran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,14 @@
 
 # include <iostream>
 # include <string>
+# include <cmath>
 # include <map> // for std::map
 # include <fstream> // for std::ifstream
 # include <sstream> // for std::istringstream
 
 class BitcoinExchange
 {
+    // _exchangeRates is a map that stores the exchange rates for each date.
     std::map<std::string, std::string> _exchangeRates;
     
     public:
